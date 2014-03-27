383ac3daebe8027dd798259943584fb06dc0523b
Lecvy Chan <lecvychan@gmail.com>
2014-03-27 09:27:21 -0400

    class

--- Classwork/DoubleLL.java
+++ Classwork/DoubleLL.java
@@ -11,13 +11,13 @@ public class DoubleLL <E> {
 	}
 
 	public void setData (E d) {
-	    data =d;
+	    data = d;
 	}
 	public E getData () {
 	    return data;
 	}
 	public void setNext (Node <E> n) {
-	    next =n;
+	    next = n;
 	}
 	public Node <E> getNext() {
 	    return next;
@@ -42,10 +42,22 @@ public class DoubleLL <E> {
 		n.setPrev(current.prev);
 		current.prev.setNext(n);
 	    }
+	    if (current.prev == null && current.next == null){
+		n.setPrev(current);
+		current.setNext(n);
+	    }
 	    n.next = current;
 	    current.prev = n;
 	    current = n;
 	}
+    }
+
+    public void delete () {
+	Node <E> tmp = current.prev;
+	Node <E> N = current.next;
+	tmp.setNext(N);
+	N.setPrev(tmp);
+	current = N;
     }
     public E getCurrent() {
 	return current.getData();
@@ -54,7 +66,7 @@ public class DoubleLL <E> {
     public void forward () {
 	if (current.getNext() != null)
 	    current = current.getNext();
-	System.out.println ("forward: " + current);
+	//System.out.println ("forward: " + current);
     }
     public void back() {
 	if (current.getPrev() != null)
@@ -62,23 +74,26 @@ public class DoubleLL <E> {
     }
 
     public String toString () {
+	Node <E> n = current;
 	if (current == null) 
 	    return "";
-	while (current.getPrev() != null) 
-	    current = current.getPrev();
-
-	Node <E> tmp = current;
+	/*
+	while (n.getPrev() != null) 
+	    n = n.getPrev();
+	*/
+	Node <E> tmp = n.getNext();
 	String s = "";
-	while (tmp != null) {
+	while (tmp != n) {
 	    s = s + tmp.getData() + " ";
 	    tmp = tmp.getNext();
 	}
+	s = n.getData() + " " +  s;
 	return s;
     }
 
     public static void main (String [] args) {
 	DoubleLL <String> L = new DoubleLL<String> ();
-	System.out.println (L);
+	/*	System.out.println (L);
 	L.insert("hello");
 	System.out.println (L);
 	L.insert("world");
@@ -92,6 +107,31 @@ public class DoubleLL <E> {
 	System.out.println (L.getCurrent());
 	L.insert ("inserted");
 	System.out.println (L);
+	System.out.println (L.getCurrent());
+	L.forward();
+	L.forward();
+	L.forward();
+	L.forward();
+	System.out.println (L.getCurrent());
+	L.insert ("hi");
+	System.out.println (L);
+	System.out.println (L.getCurrent());*/
+	L.insert("day");
+	L.insert("time");
+       	System.out.println (L.getCurrent());
+	System.out.println(L);
+	L.insert ("night");
+	L.insert("afternoon");
+ 	System.out.println ("current: " + L.getCurrent());
+	L.forward();
+	L.forward();
+ 	System.out.println ("current: " + L.getCurrent());
+	System.out.println(L);
+	System.out.println("Delete: " + L.getCurrent());
+	L.delete();
+
+	System.out.println(L);
+ 	System.out.println ("current: " + L.getCurrent());
 
     }
 }

50381387d7bdff2d059a5ad6dbbe6859f0b31720
lecvy <lecvy@lecvy-VirtualBox.(none)>
2014-04-07 00:28:56 -0400

    Stack

---
+++ 12-Stack/MyStack.java
@@ -0,0 +1,52 @@
+public class MyStack {
+
+    private String[] stack;
+    private int top;
+
+    public MyStack(){
+	stack = new String [10];
+	top = -1;
+    }
+
+    public void grow () {
+	String [] times2 = new String [stack.length * 2];
+	for (int i = 0; i <stack.length; i++) 
+	    stack = times2;
+    }
+
+    public void push(String s){
+	top ++;
+	while (top >= stack.length) 
+	    this.grow();
+	stack[top] = s;
+    }
+
+    public String pop() {
+	String ret = new String (stack[top]);
+	stack[top] = null;
+	top --;
+	return ret;
+    }
+
+    public String peek() {
+	return stack[top];
+
+    }
+      
+    public int size() {
+	return top + 1;
+    }
+
+    public boolean isEmpty() {
+	return stack[top]==null;
+    }
+
+    public String toString() {
+	String s="";
+	
+	for (int i = top; i >= 0; i--)
+	    s += stack[i] + ", ";
+    
+	return s;
+    }
+}
